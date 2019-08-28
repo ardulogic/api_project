2135b0e5891909a264bc1c16ac9c7d9a789e90da
ardulogic <denncath@gmail.com>
2019-08-28 11:55:00 +0300

    Reformatted code

--- app/classes/Users/User.php
+++ app/classes/Users/User.php
@@ -2,12 +2,11 @@
 
 namespace App\Users;
 
-class User
-{
+class User {
+
     private $data = [];
 
-    public function __construct($data = null)
-    {
+    public function __construct($data = null) {
         if ($data) {
             $this->setData($data);
         } else {
@@ -18,38 +17,32 @@ class User
         }
     }
 
-    public function setData($array)
-    {
+    public function setData($array) {
         $this->setEmail($array['email'] ?? null);
         $this->setPassword($array['password'] ?? null);
     }
 
-    public function getData()
-    {
+    public function getData() {
         return [
             'email' => $this->getEmail(),
             'password' => $this->getPassword()
         ];
     }
 
-
-    public function setEmail(String $email)
-    {
+    public function setEmail(String $email) {
         $this->data['email'] = $email;
     }
 
-    public function setPassword(String $password)
-    {
+    public function setPassword(String $password) {
         $this->data['password'] = $password;
     }
 
-    public function getEmail()
-    {
+    public function getEmail() {
         return $this->data['email'];
     }
 
-    public function getPassword()
-    {
+    public function getPassword() {
         return $this->data['password'];
     }
+
 }
